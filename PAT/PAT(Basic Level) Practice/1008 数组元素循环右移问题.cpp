111310a272539ad7a386bc727211c5830e1ba1fc
zeki <zeki_you@foxmail.com>
2022-08-08 14:48:01 +0800

    1008 数组元素循右移问题

---
+++ PAT/PAT(Basic Level) Practice/1008 数组元素循环右移问题.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int count,n;
+
+    cin >> count;
+    cin >> n;
+
+    vector<int> v;
+
+    for (int i = 0; i < count; i++)
+    {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+
+    vector<int> ret(v.size());
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        ret[(i + n) % v.size()] = v[i];
+    }
+
+    for (int i = 0; i < ret.size(); i++)
+    {
+        if (i != 0)
+        {
+            cout << " ";
+        }
+        
+            cout << ret[i];
+    }
+    
+    
+    return 0;
+}
