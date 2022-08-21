07d8f794d15630897162455d363da172a7244ec2
zeki <zeki_you@foxmail.com>
2022-08-21 17:00:26 +0800

    PAT Basic 1030 完美数列

---
+++ PAT/PAT(Basic Level) Practice/1030 完美数列.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int count, p;
+
+    cin >> count >> p;
+
+    vector<long long> v;
+
+    for (size_t i = 0; i < count; i++)
+    {
+        long long n;
+        cin >> n;
+        v.push_back(n);
+    }
+
+    sort(v.begin(), v.end());
+
+    int max = 0;
+
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        
+        int sum= 0; 
+
+        for (size_t k = i; k < count; k++)
+        {
+            
+            if (v[k] > v[i] * p)
+            {
+                break;
+            }
+
+            sum++; 
+        }
+
+        if (sum > max)
+        {
+            max = sum;
+        }
+        
+    }
+
+    cout << max;
+    
+    
+    return 0;
+}
