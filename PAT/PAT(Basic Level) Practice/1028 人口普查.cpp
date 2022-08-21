12399e15b9e6c56d9c597068a3020e95514a6dee
zeki <zeki_you@foxmail.com>
2022-08-21 11:31:59 +0800

    PAT Basic 1028 人口普查

--- PAT/PAT(Basic Level) Practice/1028 人口普查.cpp
+++ PAT/PAT(Basic Level) Practice/1028 人口普查.cpp
@@ -11,117 +11,54 @@ typedef struct person
     string date;
 } person;
 
-bool cmp(person p1, person p2)
-// 2022/08/12
-{
-
-    string str1 = p1.date;
-
-    int year1 = atoi(str1.substr(0, 4).c_str());
-
-    int month1 = atoi(str1.substr(5, 2).c_str());
-
-    int day1 = atoi(str1.substr(8, 2).c_str());
-
-    string str2 = p2.date;
-
-    int year2 = atoi(str2.substr(0, 4).c_str());
-
-    int month2 = atoi(str2.substr(5, 2).c_str());
-
-    int day2 = atoi(str2.substr(8, 2).c_str());
-
-    if (year1 != year2)
-    {
-        return year1 > year2;
-    }
-
-    if (month1 != month2)
-    {
-        return month1 > month2;
-    }
-
-    if (day1 != day2)
-    {
-        return day1 > day2;
-    }
-}
-
-bool isRealDate(string str)
-{
-    int year = atoi(str.substr(0, 4).c_str());
-
-    int month = atoi(str.substr(5, 2).c_str());
-
-    int day = atoi(str.substr(8, 2).c_str());
-
-    if (year > 2014)
-    {
-        return false;
-    }
-
-    if (year < 1814)
-    {
-        return false;
-    }
-
-    if (year == 2014 && month > 9)
-    {
-        return false;
-    }
-
-    if (year == 2014 && month == 9 && day > 6)
-    {
-        return false;
-    }
-
-    if (year == 1814 && month < 9)
-    {
-        return false;
-    }
-
-    if (year == 1814 && month == 9 && day < 6)
-    {
-        return false;
-    }
-
-    return true;
-}
-
 int main(int argc, char const *argv[])
 {
     int count;
 
-    vector<person> v;
+    person old;
+    old.date = "2014/09/06";
+    person young;
+    young.date = "1814/09/06";
 
-    int ret = 0;
+    int sum = 0;
 
     cin >> count;
 
     for (size_t i = 0; i < count; i++)
     {
+        string name , date;
+        cin >> name;
+        cin >> date;
 
-        string name;
-        string date;
-
-        cin >> name >> date;
-
-        if (isRealDate(date))
+        if (date > "2014/09/06" || date < "1814/09/06")
         {
-            person p;
-            p.name = name;
-            p.date = date;
-            v.push_back(p);
+            continue;
         }
+        
+        sum++;
+
+        if (date > young.date)
+        {
+            young.name = name;
+            young.date = date;
+        }
+
+        if (date < old.date)
+        {
+            old.name = name;
+            old.date = date;
+        }
+        
     }
 
-    sort(v.begin(), v.end(), cmp);
-
-    if (v.size() != 0)
+    if (sum == 0)
     {
-        cout << v.size() << " " << v.rbegin()->name << " " << v.begin()->name << endl;
-    } else
         cout << 0;
+        return 0;
+    }
+    
+    cout << sum << " " << old.name << " " << young.name << endl;
+    
 
     return 0;
 }
