b3ebe3ab1140242052975ccaa8b38c0dd20e4bc0
zeki <zeki_you@foxmail.com>
2022-09-20 15:36:33 +0800

    PAT Basic 1066 图像过滤

---
+++ PAT/PAT(Basic Level) Practice/1066 图像过滤.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int m, n, a, b, k;
+    scanf("%d %d %d %d %d", &m, &n, &a, &b, &k);
+
+    cout << setfill('0');
+
+    for (size_t i = 0; i < m; i++)
+    {
+        for (size_t i = 0; i < n; i++)
+        {
+            int n;
+            cin >> n;
+
+            if (i != 0)
+            {
+                cout << " ";
+            }
+
+            if (a <= n && n <= b)
+            {
+                cout << setw(3) << k;
+            }
+            else
+            {
+                cout << setw(3) << n;
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
