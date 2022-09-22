1b1e9189fd1c067ee3ced7e328836cdd5a7552dc
zeki <zeki_you@foxmail.com>
2022-09-22 17:24:24 +0800

    PAT Basic 1067 试密码

--- PAT/PAT(Basic Level) Practice/1067 试密码.cpp
+++ PAT/PAT(Basic Level) Practice/1067 试密码.cpp
@@ -10,32 +10,37 @@ int main(int argc, char const *argv[])
     cin >> password >> count;
 
     getchar();
-    for (size_t i = 0; i < count; i++)
-    {
+    
+    int n = 0;
+    while(1) {
         string s;
         getline(cin, s);
+        
+            
+        if (n == count)
+        {
+            cout << "Account locked" << endl;
+            break;
+        }
 
         if (s == "#")
         {
             break;
         }
-
-        if (s != password)
+        
+        if (s != password && n <= count)
         {
             cout << "Wrong password: " << s << endl;
         }
         
-        if (s == password)
+        if (s == password && n <= count)
         {
             cout << "Welcome in" << endl;
-            goto END;
+            break;
         }
         
-        
+        n++;
     }
 
-    cout << "Account locked" << endl;
-
-END:
     return 0;
 }
