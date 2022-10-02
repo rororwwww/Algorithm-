d13e6c0d9562acd1b2e8a508368b4686d06ecd9c
zeki <zeki_you@foxmail.com>
2022-10-02 22:11:25 +0800

    PAT Basic 1072 开学寄语

--- PAT/PAT(Basic Level) Practice/1072 开学寄语.cpp
+++ PAT/PAT(Basic Level) Practice/1072 开学寄语.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,6 +19,9 @@ int main(int argc, char const *argv[])
         forbid.push_back(s);
     }
 
+    int countA = 0;
+    int countB = 0;
+
     for (size_t i = 0; i < n; i++)
     {
         string name;
@@ -25,21 +30,52 @@ int main(int argc, char const *argv[])
 
         bool isforbid = false;
         bool isName = true;
+
+        bool firstForbid = true;
         
         for (size_t i = 0; i < count; i++)
         {
+            string s;
+
+            cin >> s;
+
+            isforbid = false;
+
+            if (find(forbid.begin(), forbid.end(), s) != forbid.end())
+            {
+                isforbid = true;
+            }
             
             if (isforbid && isName)
             {
-                cout << ""
+                cout << name << ": ";
+                countA++;
+                isName = false;
             }
+
+            if (isforbid)
+            {
+                if (!firstForbid)
+                {
+                    cout << " ";
+                }
+                
+                cout << setfill('0');
+                cout << setw(4) << s;
+                countB++;
+                firstForbid = false;
+            }
+            
         }   
         
-       
+        if (!isName)
+        {
+            cout << endl;
+        }
         
-
     }
     
-    
+    cout << countA << " " << countB << endl;
+
     return 0;
 }
