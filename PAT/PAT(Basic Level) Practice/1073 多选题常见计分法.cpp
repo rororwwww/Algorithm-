0e9f89186e23740f5868fb941f97a5c4f0225e87
zeki <zeki_you@foxmail.com>
2022-10-03 22:19:22 +0800

    PAT Basic 1073 多选题常见计分方法

---
+++ PAT/PAT(Basic Level) Practice/1073 多选题常见计分法.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct Task 
+{
+    int count;
+    string answer;
+}Task;
+
+
+int main(int argc, char const *argv[])
+{
+    int n,m;
+    cin >> n >> m;
+
+
+
+    return 0;
+}
