4e984c18a9007957da405ca9c4de4561d11ccb21
zeki <zeki_you@foxmail.com>
2022-10-10 14:58:21 +0800

    PAT Basic 1073 多选题常见计分法

--- PAT/PAT(Basic Level) Practice/1073 多选题常见计分法.cpp
+++ PAT/PAT(Basic Level) Practice/1073 多选题常见计分法.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-typedef struct Task 
+typedef struct Task
 {
-    int count;
+    int score;
     string answer;
 }Task;
 
@@ -15,7 +16,58 @@ int main(int argc, char const *argv[])
     int n,m;
     cin >> n >> m;
 
+    vector<Task> v;
 
+    for (size_t i = 0; i < m; i++)
+    {
+        Task t;
+
+        int score;
+        cin >> score;
+        
+        t.score = score;
+
+        int count, anscount;
+        cin >> count >> anscount;
+
+        string s;
+
+        for (size_t i = 0; i < anscount; i++)
+        {
+            char c;
+            cin >> c;
+            s += c;
+        }
+
+        t.answer = s;
+
+        v.push_back(t);
+        
+    }
+    
+    for (size_t i = 0; i < n; i++)
+    {
+
+        for (size_t i = 0; i < m; i++)
+        {
+            getchar();
+            
+            int count;
+            scanf("(%d", &count);
+
+            string ans;
+
+            for (size_t i = 0; i < count; i++)
+            {
+                char c;
+                scanf(" %c)", &c);
+                ans += c;
+            }
+
+        } 
+
+    }
+    
 
     return 0;
 }
