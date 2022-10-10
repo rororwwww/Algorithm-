8e312103cae02783514e407b3b6d1447e45e8335
zeki <zeki_you@foxmail.com>
2022-10-10 17:22:47 +0800

    PAT Basic 1073 多选题常见计分法

--- PAT/PAT(Basic Level) Practice/1073 多选题常见计分法.cpp
+++ PAT/PAT(Basic Level) Practice/1073 多选题常见计分法.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,15 +9,17 @@ typedef struct Task
 {
     int score;
     string answer;
-}Task;
-
+    int WrongCount = 0;
+    int WrongVector[5] = {0};
+} Task;
 
 int main(int argc, char const *argv[])
 {
-    int n,m;
+    int n, m;
     cin >> n >> m;
 
     vector<Task> v;
+    vector<float> student(n, 0);
 
     for (size_t i = 0; i < m; i++)
     {
@@ -24,7 +27,7 @@ int main(int argc, char const *argv[])
 
         int score;
         cin >> score;
-        
+
         t.score = score;
 
         int count, anscount;
@@ -42,16 +45,15 @@ int main(int argc, char const *argv[])
         t.answer = s;
 
         v.push_back(t);
-        
     }
-    
-    for (size_t i = 0; i < n; i++)
+
+    for (size_t k = 0; k < n; k++)
     {
 
         for (size_t i = 0; i < m; i++)
         {
             getchar();
-            
+
             int count;
             scanf("(%d", &count);
 
@@ -64,10 +66,113 @@ int main(int argc, char const *argv[])
                 ans += c;
             }
 
-        } 
+            float score = 0.0;
 
+            bool flag = true;
+
+            for (size_t m = 0; m < ans.size(); m++)
+            {
+                // 错误
+                if (v[i].answer.find(ans[m]) == string::npos)
+                {
+                    score = 0.0;
+                    flag = false;
+                    v[i].WrongVector[ans[m] - 'a']++;
+                }
+            }
+
+            //漏选排查
+            for (size_t n = 0; n < v[i].answer.size(); n++)
+            {
+                if (ans.find(v[i].answer[n]) == string::npos)
+                {
+                    v[i].WrongVector[ v[i].answer[n] - 'a']++;
+                }
+                
+            }
+
+            if (flag)
+            {
+                //满分
+                if (v[i].answer.size() == ans.size())
+                {
+                    score = v[i].score;
+                }
+                //半对
+                else
+                {
+                    score = v[i].score / 2.0;
+                }
+            }
+
+            student[k] += score;
+        }
+    }
+
+    //输出均分
+    for (size_t i = 0; i < student.size(); i++)
+    {
+        cout << fixed << setprecision(1) << student[i] << endl;
+    }
+
+    //每个题WrongCount统计
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        int max = 0;
+        for (size_t n = 0; n < 5; n++)
+        {
+            if (max < v[i].WrongVector[n])
+            {
+                max = v[i].WrongVector[n];
+            }
+        }
+
+        v[i].WrongCount = max;
     }
     
+    //寻找错误数量最多的题
+    int max = 0;
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        if (max < v[i].WrongCount)
+        {
+            max = v[i].WrongCount;
+        }
+    }
+
+    if (max == 0)
+    {
+        cout << "Too simple" << endl;
+    }
+    else
+    {
+        for (size_t i = 0; i < v.size(); i++)
+        {
+            if (v[i].WrongCount == max)
+            {
+                //寻找错最多的选项
+                int max = 0;
+                for (size_t n = 0; n < 5; n++)
+                {
+                    if (max < v[i].WrongVector[n])
+                    {
+                        max = v[i].WrongVector[n];
+                    }
+                }
+
+                for (size_t n = 0; n < 5; n++)
+                {
+                    if (max == v[i].WrongVector[n])
+                    {
+                        cout << max << " ";
+                        char c = 'a' + n;
+                        cout << i + 1 << "-" << c << endl;
+                    }
+                }
+
+            }
+        }
+    }
 
     return 0;
 }
