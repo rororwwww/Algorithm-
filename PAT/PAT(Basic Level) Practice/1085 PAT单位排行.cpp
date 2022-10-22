aabb95ad94155e544054f89a2c32537014849abb
zeki <zeki_you@foxmail.com>
2022-10-22 22:30:30 +0800

    PAT Basic 1085 PAT单位排行

---
+++ PAT/PAT(Basic Level) Practice/1085 PAT单位排行.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+// 准考证号 得分 学校
+struct Student
+{
+    string id;
+    int score;
+    string school;
+};
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int count;
+    cin >> count;
+    
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
